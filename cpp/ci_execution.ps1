3248957d62b1d41a02bbfe79cb279c389d8c3ae2
Xavier Bonaventura <xavibonaventura@gmail.com>
2021-09-28 21:56:13 +0200

    Print variables

--- cpp/ci_execution.ps1
+++ cpp/ci_execution.ps1
@@ -7,13 +7,9 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-Location $PSScriptRoot
-bazel version
-if($IsWindows) {
-    Write-Host "Skipping 'bazel run //:buildifier' on Windows"
-    Write-Host "Running buildifier from the Bazel rule does not work on Windows"
-    Write-Host "See https://github.com/keith/buildifier-prebuilt/issues/99 and https://github.com/bazelbuild/bazel-central-registry/issues/380"
-} else {
-    bazel run //:buildifier
-}
-bazel build //...
-bazel run //:hello_world
+Get-ChildItem env:
+bazel build //:hello_world --subcommands=pretty_print
+whereis gcc-12
+whereis gcc
+ls -ld /usr/bin/*gcc* /bin/*gcc* /home/appveyor/bin/*
+ls -ld /usr/bin/*ld* /bin/*ld* /home/appveyor/bin/*
--- cpp_coverage/ci_execution.ps1
+++ cpp_coverage/ci_execution.ps1
@@ -3,19 +3,3 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-Location $PSScriptRoot
 bazel version
-if($IsWindows) {
-    Write-Host "Skipping 'bazel run //:buildifier' on Windows"
-    Write-Host "Running buildifier from the Bazel rule does not work on Windows"
-    Write-Host "See https://github.com/keith/buildifier-prebuilt/issues/99 and https://github.com/bazelbuild/bazel-central-registry/issues/380"
-} else {
-    bazel run //:buildifier
-}
-bazel build //...
-bazel test //:foo_test
-if($IsWindows) {
-    Write-Host "Skipping 'bazel coverage //:foo_test' on Windows"
-    Write-Host "Bazel coverage is not supported on Windows"
-    Write-Host "See https://github.com/bazelbuild/bazel/issues/6374"
-} else {
-    bazel coverage //:foo_test
-}
