eb9d9dda67b7e09ca1c3ddfe1f97701898736f57
Xavier Bonaventura <xavibonaventura@gmail.com>
2023-12-31 10:07:51 +0100

    Reduce required version of powershell to 7.3

--- cpp/ci_execution.ps1
+++ cpp/ci_execution.ps1
@@ -1,4 +1,8 @@
-#Requires –Version 7.4
+#Requires –Version 7.3
+If ($PSVersionTable.PSVersion -lt [version]"7.4") {
+    Write-Host "Powershell version lower than 7.4. We need to enable experimental feature PSNativeCommandUseErrorActionPreference"
+    Enable-ExperimentalFeature PSNativeCommandErrorActionPreference
+}
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
