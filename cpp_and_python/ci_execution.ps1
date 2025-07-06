10aea6155230c7c90bc894b3bf52b5d7072d07a4
Xavier Bonaventura <xavibonaventura@gmail.com>
2025-07-06 11:14:08 +0200

    Update C++ and Python example to use Bzlmod and Bazel 8

--- cpp_and_python/ci_execution.ps1
+++ cpp_and_python/ci_execution.ps1
@@ -2,13 +2,15 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-Location $PSScriptRoot
-if($IsWindows) {
+if ($IsWindows) {
     Write-Host "Skipping 'cpp_and_python' example on Windows"
     Write-Host "It is not working in the Appveyor CI because it is not properly setup"
-} else {
+}
+else {
     bazel version
     bazel run //:buildifier
     bazel build //...
     bazel run //:hello_world_everyone
     bazel run //:hello_world_code_dive
+    bazel run //:hello_world_ndc_techtown
 }
