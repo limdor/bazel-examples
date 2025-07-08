76ba3444bf7d2fde032731d89d01b6af2dea7394
Xavier Bonaventura <xavibonaventura@gmail.com>
2025-07-08 19:35:52 +0200

    Revert "Enable buildifier on Windows"
    This reverts commit 844f3b2dc28fa112c7cb9d2adc97baa41c524cb8.

--- third_party_dependencies/ci_execution.ps1
+++ third_party_dependencies/ci_execution.ps1
@@ -3,6 +3,12 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-Location $PSScriptRoot
 bazel version
-bazel run //:buildifier
+if($IsWindows) {
+    Write-Host "Skipping 'bazel run //:buildifier' on Windows"
+    Write-Host "Running buildifier from the Bazel rule does not work on Windows"
+    Write-Host "See https://github.com/bazelbuild/buildtools/issues/346 and https://github.com/bazelbuild/buildtools/issues/770"
+} else {
+    bazel run //:buildifier
+}
 bazel build //...
 bazel test //...
