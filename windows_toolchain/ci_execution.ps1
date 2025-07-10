4d304ad4771d51c583536bb5e2c6ccd20175f410
Xavier Bonaventura <xavibonaventura@gmail.com>
2025-07-10 22:34:54 +0200

    Run in CI only with Visual Studio 2022

--- windows_toolchain/ci_execution.ps1
+++ windows_toolchain/ci_execution.ps1
@@ -9,7 +9,7 @@ if ($IsWindows) {
     Write-Host "Skipping 'bazel run //:buildifier' on Windows"
     Write-Host "Running buildifier from the Bazel rule does not work on Windows"
     Write-Host "See https://github.com/keith/buildifier-prebuilt/issues/99 and https://github.com/bazelbuild/bazel-central-registry/issues/380"
-    if ($Env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2019") {
+    if ($Env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2022") {
         bazel build //...
         bazel build //... --platforms=//platform:windows_x64
         bazel run //:hello_world
